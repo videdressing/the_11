0955149ec15243b834bb43e389ec43a3c1b34735
David Piry <davidpiry@gmail.com>
2017-07-09 14:30:24 +0200

    Handles pictures now

--- src/components/Venue.vue
+++ src/components/Venue.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="col-12 col-md-9 venue">
     <h1>{{ venue.name }} ( {{ $route.params.id }} )</h1>
-    <!-- img v-attr="src: venue.pictures" /-->
+    <ul>
+      <li v-for="picture in venue.pictures">
+        <img v-bind:src="picture" />
+      </li>
+    </ul>
     <p>{{ venue.description }}</p>
   </div>
 </template>

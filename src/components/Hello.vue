7f313da6bf0e1f7053d4eb18d0d1dc0b68f99fa3
David Piry <davidpiry@gmail.com>
2018-01-30 13:49:30 +0100

    Algolia search optimisations

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,27 +1,49 @@
 <template>
-  <b-row>
-    <b-col cols="12" md="6" lg="4" v-for="venue in venues">
-      <router-link :to="{name: 'VenueView', params: { key: venue['.key'] }}">
-        <b-card overlay
-                :img-src="getThumb(venue.pictures[0].url)"
-                :img-alt="venue.name"
-                text-variant="white"
-                :title="venue.name"
-                class="text-center mb-3">
-        </b-card>
-      </router-link>
-    </b-col>
-  </b-row>
+  <b-container>
+    <b-row>
+      <b-col>
+        <ais-search-box :search-store="searchStore" class="mb-3">
+            <ais-input :search-store="searchStore"
+              placeholder="Search for venue"
+              :class-names="{'ais-input': 'form-control'}"
+            >
+            </ais-input>
+        </ais-search-box>
+      </b-col>
+    </b-row>
+
+    <b-row>
+      <ais-index :search-store="searchStore" index-name="venues">
+          <ais-results inline-template>
+            <div>
+              <b-col cols="12" md="6" lg="4" v-for="result in results" :key="result.objectID">
+                <router-link :to="{name: 'VenueView', params: { key: result.objectID }}">
+                  <b-card overlay
+                          :img-src="result.pictures[0].secure_url.replace(/v[0-9]+/, 'w_600,h_400,c_fill')"
+                          :img-alt="result.name"
+                          text-variant="white"
+                          :title="result.name"
+                          class="text-center mb-3">
+                  </b-card>
+                </router-link>
+              </b-col>
+            </div>
+          </ais-results>
+      </ais-index>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
 import {db} from '../initFirebase.js'
+import { createFromAlgoliaCredentials } from 'vue-instantsearch'
 
 export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to The_11'
+      msg: 'Welcome to The_11',
+      searchStore: createFromAlgoliaCredentials('XJ4KFZWO2I', 'b29f97abde99f1dcd9874e03ee0807fc')
     }
   },
   computed: {
@@ -33,11 +55,6 @@ export default {
     dbVenues: {
       source: db.ref('venues').limitToLast(6),
       asObject: false
-    }
-  },
-  methods: {
-    getThumb: function (url) {
-      return url.replace(/v[0-9]+/, 'w_600,h_400,c_fill')
     }
   }
 }
--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -23,55 +23,13 @@
         <b-nav-item :to="{name: 'VenueAdd'}">Add Venue</b-nav-item>
       </b-nav>
 
-      <b-nav is-nav-bar class="ml-auto">
-          <ais-search-box :search-store="searchStore">
-            <b-input-group>
-              <ais-input :search-store="searchStore"
-                placeholder="Search for venue"
-                :class-names="{'ais-input': 'form-control'}"
-                id="popoverResults"
-              >
-              </ais-input>
-            </b-input-group>
-          </ais-search-box>
-      </b-nav>
-
-      <ais-index :search-store="searchStore" index-name="venues">
-        <b-popover target="popoverResults" triggers="click" placement="auto">
-          <ais-results inline-template>
-            <b-list-group>
-              <b-list-group-item v-for="result in results" :key="result.objectID">
-                <router-link
-                  :to="{name: 'VenueView', params: { key: result.objectID }}"
-                  role="button"
-                >
-                  {{ result.name }}
-                </router-link>
-              </b-list-group-item>
-            </b-list-group>
-          </ais-results>
-        </b-popover>
-      </ais-index>
-
     </b-collapse>
   </b-navbar>
 </template>
 
 <script>
-import { createFromAlgoliaCredentials } from 'vue-instantsearch'
-
 export default {
-  name: 'main-menu',
-  data () {
-    return {
-      searchStore: createFromAlgoliaCredentials('XJ4KFZWO2I', 'b29f97abde99f1dcd9874e03ee0807fc')
-    }
-  },
-  methods: {
-    format (value) {
-      return value.toLowerCase()
-    }
-  }
+  name: 'main-menu'
 }
 </script>
 

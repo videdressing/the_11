bfb1a3f498d3b7a1117aaed73a5f3821d12c1104
David Piry <davidpiry@gmail.com>
2018-04-10 13:41:35 +0200

    Nice Google Maps for Venues

--- src/components/Venue/Preview.vue
+++ src/components/Venue/Preview.vue
@@ -10,19 +10,52 @@
         </b-carousel>
       </b-col>
       <b-col sm="12" lg="6">
-        <p v-if="venue.address">{{ venue.address.postal_code }}</p>
         <pre>{{ venue.description }}</pre>
+        <gmap-map
+          :center="center"
+          :zoom="15"
+          style="width: 100%; height: 300px"
+        >
+          <gmap-marker
+            :position="center"
+            :clickable="false"
+            :draggable="false"
+          ></gmap-marker>
+        </gmap-map>
+        <p v-if="venue.address">
+          {{ venue.address.street_number }}
+          {{ venue.address.route }},
+          {{ venue.address.postal_code }}
+        </p>
       </b-col>
     </b-row>
   </div>
 </template>
 
 <script>
+import * as VueGoogleMaps from 'vue2-google-maps'
+import Vue from 'vue'
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyDoyPRWSXuUu9WWel323GQ1UnSc-gp8zCs',
+    libraries: 'places' // This is required if you use the Autocomplete plugin
+    // OR: libraries: 'places,drawing'
+    // OR: libraries: 'places,drawing,visualization'
+    // (as you require)
+  }
+})
+
 export default {
   props: ['venue'],
   methods: {
     getThumb: function (url) {
       return url.replace(/v[0-9]+/, 'w_1200,h_800,c_fill')
+    }
+  },
+  data () {
+    return {
+      center: {lat: this.venue.address.latitude, lng: this.venue.address.longitude}
     }
   }
 }

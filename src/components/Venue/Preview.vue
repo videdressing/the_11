56346446547f5cdd1df7ad57d59c3b788eaba72c
David Piry <davidpiry@gmail.com>
2018-04-10 13:20:18 +0200

    Desktop compatibility for Venues

--- src/components/Venue/Preview.vue
+++ src/components/Venue/Preview.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
     <h1>{{ venue.name }}</h1>
-
-    <b-carousel id="carousel1"
-                style="text-shadow: 1px 1px 2px #333;"
-                indicators
-                background="#ababab"
-                :interval="4000"
-    >
-      <b-carousel-slide v-for="picture in venue.pictures" :img-src="getThumb(picture.url)">
-      </b-carousel-slide>
-    </b-carousel>
-
-    <p v-if="venue.address">{{ venue.address.postal_code }}</p>
-    <pre>{{ venue.description }}</pre>
+    <b-row>
+      <b-col sm="12" lg="6">
+        <b-carousel id="carousel1" style="text-shadow: 1px 1px 2px #333;" indicators 
+          background="#ababab" :interval="4000">
+          <b-carousel-slide v-for="picture in venue.pictures"
+            :img-src="getThumb(picture.url)"></b-carousel-slide>
+        </b-carousel>
+      </b-col>
+      <b-col sm="12" lg="6">
+        <p v-if="venue.address">{{ venue.address.postal_code }}</p>
+        <pre>{{ venue.description }}</pre>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
